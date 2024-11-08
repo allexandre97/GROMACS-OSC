5399e22b0b9a10aed1123c237c9121f56663736a
Xandre <allexandre97@gmail.com>
2024-11-08 09:43:12 +0100

    Cleaned up compile script

--- compile_gromacs.sh
+++ compile_gromacs.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 
-while getopts 'f:p:j:' OPTION; do
+# Default values
+FREQ=12
+AMPL=35
+INSTALL_PATH="${HOME}/gromacs-osc/"
+JOBS=1
 
-	case "$OPTION" in
-
-		f)
-			echo "SETTING FREQUENCY TO ${OPTARG} MHz"
-			FREQ=${OPTARG}
-			sed "s/FREQ/${OPTARG}/g" ./gromacs-osc/src/gromacs/mdlib/coupling_0 > ./gromacs-osc/src/gromacs/mdlib/coupling.cpp 
-			;;
-		p)
-			echo "WILL INSTALL IN PATH: ${OPTARG}"
-			INSTALL_PATH=${OPTARG}
-			;;
-		j)
-			echo "WILL USE ${OPTARG} JOBS"
-			JOBS=${OPTARG}
-			;;
-		?)
-			echo "Usage: $(basename $0) [-f <frequency>] [-p <install path>]"
-			exit 1
-			;;
-	esac
+while getopts 'f:a:p:j:' OPTION; do
+    case "$OPTION" in
+        f)
+            echo "SETTING FREQUENCY TO ${OPTARG} MHz"
+            FREQ=${OPTARG}
+            ;;
+        a)
+            echo "SETTING AMPLITUDE TO ${OPTARG} bar"
+            AMPL=${OPTARG}
+            ;;
+        p)
+            echo "WILL INSTALL IN PATH: ${OPTARG}"
+            INSTALL_PATH=${OPTARG}
+            ;;
+        j)
+            echo "WILL USE ${OPTARG} JOBS"
+            JOBS=${OPTARG}
+            ;;
+        ?)
+            echo "Usage: $(basename $0) [-f <frequency>] [-a <amplitude>] [-p <install path>] [-j <jobs>]"
+            exit 1
+            ;;
+    esac
 done
 
 module load cesga/2020 gcc/system openmpi/4.1.4_ft3
+
+# First substitute FREQ
+sed "s/FREQ/${FREQ}/g" ./gromacs-osc/src/gromacs/mdlib/coupling_0 > ./gromacs-osc/src/gromacs/mdlib/coupling_tmp
+
+# Then substitute AMPL
+sed "s/AMP/${AMPL}/g" ./gromacs-osc/src/gromacs/mdlib/coupling_tmp > ./gromacs-osc/src/gromacs/mdlib/coupling.cpp
+
+# Remove temporary file
+rm ./gromacs-osc/src/gromacs/mdlib/coupling_tmp
 
 cd gromacs-osc
 rm -rf build
@@ -35,4 +51,3 @@ cmake .. -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpic++ -DGMX_MPI=ON -DCMA
 make -j ${JOBS}
 
 make install
-
